337c519280a8c6b073b755372d96bc2bd1ae1cb3
Andrii Nastych <andrii.nastych@gmail.com>
2026-01-17 15:39:52 +0100

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import ContactCTA from './components/ContactCTA';
+import { Analytics } from "@vercel/analytics/react";
 
 
 function App() {
   return (
     <div className="App">
+       <Analytics/>
       <Header />
       <Services />
       <Portfolio />
